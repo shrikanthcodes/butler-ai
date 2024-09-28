31bd91d6960e33bf122233bc4a484ef9e73e155c
Shrikanth Subramanian <shrikanthsubramanian@gmail.com>
2024-09-28 18:56:56 -0400

    Added openai summary API/updated chat_complete API

--- backend/pkg/api/external/openai/chat_complete.go
+++ backend/pkg/api/external/openai/chat_complete.go
@@ -6,30 +6,36 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
-func ChatComplete(ai_model string, api_key string, conversation []model.Dialogue) model.OpenAIResponse {
+func ChatComplete(ai_model string, api_key string, conversation []model.Dialogue, max_tokens int, temperature float32) (model.OpenAIResponse, error) {
 
 	url := "https://api.openai.com/v1/chat/completions"
 
 	// Convert the conversation slice to JSON
 	conversationJSON, err := json.Marshal(conversation)
 	if err != nil {
-		fmt.Println("Error marshalling conversation:", err)
-		return model.OpenAIResponse{}
+		log.Fatalf("Error marshalling conversation: %s", err)
+	}
+
+	// Check if any of the input is invalid
+	if string(conversationJSON) == "" || ai_model == "" || api_key == "" || max_tokens == 0 {
+		log.Fatalf("Wrong function parameters for summarization, check again")
 	}
 
 	// Construct the payload
 	payload := fmt.Sprintf(`{
 		"model": "%s",
-		"messages": %s
-	}`, ai_model, conversationJSON)
+		"messages": %s,
+		"max_tokens: %d,
+		"temperature": %f
+	}`, ai_model, conversationJSON, max_tokens, temperature)
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(payload)))
 	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return model.OpenAIResponse{}
+		log.Fatalf("Error creating request: %s", err)
 	}
 
 	req.Header.Set("Content-Type", "application/json")
@@ -38,23 +44,22 @@ func ChatComplete(ai_model string, api_key string, conversation []model.Dialogue
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error making request:", err)
-		return model.OpenAIResponse{}
+		// Returns to catch Error 429 so it can be handled gracefully
+		log.Printf("Error making request: %s", err)
+		return model.OpenAIResponse{}, err
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return model.OpenAIResponse{}
+		log.Fatalf("Error reading response body: %s", err)
 	}
 
 	var responseBody model.OpenAIResponse
 	err = json.Unmarshal(body, &responseBody)
 	if err != nil {
-		fmt.Println("Error unmarshalling response body:", err)
-		return model.OpenAIResponse{}
+		log.Fatalf("Error unmarshalling response body: %s", err)
 	}
 
-	return responseBody
+	return responseBody, err
 }
---
+++ backend/pkg/services/ai/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+type temperature float32
+
+const (
+	creative     temperature = 0.8
+	determinstic temperature = 0.2
+	regular      temperature = 0.5
+)
