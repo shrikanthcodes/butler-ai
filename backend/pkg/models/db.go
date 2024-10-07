46be4e9dd889ca6345322a26a8022a75fe070c1f
Shrikanth Subramanian <shrikanthsubramanian@gmail.com>
2024-10-07 18:51:24 -0400

    Added support for enums, modified db models

--- backend/pkg/models/db.go
+++ backend/pkg/models/db.go
@@ -1,56 +1,56 @@
 package models
 
 import (
-	"gorm.io/gorm"
+	enum "backend/pkg/models/enum"
+
+	gorm "gorm.io/gorm"
 )
 
 // 1. User
 type User struct {
 	gorm.Model
-	UserID         string `json:"user_id" gorm:"primaryKey"`
-	Name           string `json:"name"`
-	Email          string `json:"email" gorm:"unique"`
-	ProfileWriteup string `json:"profile_writeup"` // LLM generated Profile writeup for the user based on ThingsToConsider (generated each time there is a change in preferences max 1000 len)
+	UserID string `json:"user_id" gorm:"primaryKey"`
+	Name   string `json:"name"`
+	Email  string `json:"email" gorm:"unique"`
+	Phone  string `json:"phone" gorm:"unique" optional:"true"`
 
 	// Relationships to other tables
 	Auth         *Authentication `gorm:"foreignKey:UserID"` // Optional relationship
-	Choices      *Choice         `gorm:"foreignKey:UserID"` // Optional relationship
+	Writeup      *Writeup        `gorm:"foreignKey:UserID"` // Optional relationship
 	Conversation []Conversation  `gorm:"foreignKey:UserID"` // Optional relationship
 	Profile      *Profile        `gorm:"foreignKey:UserID"` // Optional relationship
 	Health       *Health         `gorm:"foreignKey:UserID"` // Optional relationship
 	Diet         *Diet           `gorm:"foreignKey:UserID"` // Optional relationship
 	Inventory    *Inventory      `gorm:"foreignKey:UserID"` // Optional relationship
 	Goal         *Goal           `gorm:"foreignKey:UserID"` // Optional relationship
-	Script       *Script         `gorm:"foreignKey:UserID"` // Optional relationship
+	Recipe       *Recipe         `gorm:"foreignKey:UserID"` // Optional relationship
 	Shopping     *Shopping       `gorm:"foreignKey:UserID"` // Optional relationship
 	MealChoices  *MealChoice     `gorm:"foreignKey:UserID"` // Optional relationship
 }
 
-type Choice struct {
+type Writeup struct {
 	gorm.Model
 	UserID string `json:"user_id" gorm:"primaryKey"` // Foreign key
-	// Optional Booleans to track which profiles are required
-	IsProfile     bool `json:"profile" gorm:"default:false"`
-	IsHealth      bool `json:"health" gorm:"default:false"`
-	IsDiet        bool `json:"diet" gorm:"default:false"`
-	IsInventory   bool `json:"inventory" gorm:"default:false"`
-	IsGoal        bool `json:"goal" gorm:"default:false"`
-	IsScript      bool `json:"script" gorm:"default:false"`
-	IsShopping    bool `json:"shopping" gorm:"default:false"`
-	IsPreferences bool `json:"preferences" gorm:"default:false"`
+	// Writeups for each of the chat types
+	RecipeWriteup         string `json:"recipe_writeup"`          // Writeup for recipe chat
+	ShoppingWriteup       string `json:"shopping_writeup"`        // Writeup for shopping chat
+	CalorieTrackerWriteup string `json:"calorie_tracker_writeup"` // Writeup for calorie tracker chat
+	HealthWriteup         string `json:"health_writeup"`          // Writeup for health chat
+	MotivationWriteup     string `json:"motivation_writeup"`      // Writeup for motivation chat
 }
 
 // 2. Conversation
 type Conversation struct {
 	gorm.Model
-	ConvID       string     `json:"conv_id" gorm:"primaryKey"`                       // Primary key
-	UserID       string     `json:"user_id"`                                         // Foreign key
-	User         User       `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Define foreign key
-	Conversation []Dialogue `json:"conversation" gorm:"type:jsonb"`                  // Actual conversation between user and AI
-	LastUpdated  string     `json:"last_updated"`                                    // Last updated timestamp
-	Summary      string     `json:"summary"`
-	Task         string     `json:"task"`
-	IsActive     bool       `json:"is_active"`
+	ConvID          string        `json:"conv_id" gorm:"primaryKey"`                       // Primary key
+	UserID          string        `json:"user_id"`                                         // Foreign key
+	User            User          `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Define foreign key
+	Conversation    []Dialogue    `json:"conversation" gorm:"type:jsonb"`                  // Actual conversation between user and AI
+	LastUpdated     string        `json:"last_updated" optional:"true"`                    // Last updated timestamp
+	Summary         string        `json:"summary" optional:"true"`                         // Summary of the conversation
+	RecentDialogues []Dialogue    `json:"recent_dialogues" gorm:"type:jsonb"`              // Recent dialogues stored as JSONB
+	ChatType        enum.ChatType `json:"chat_type"`                                       // Type of chat (e.g., recipe, shopping)
+	IsActive        bool          `json:"is_active"`
 }
 
 // 3. Health
@@ -67,30 +67,30 @@ type Health struct {
 // 4. Profile
 type Profile struct {
 	gorm.Model
-	UserID     string `json:"user_id"`                                         // Foreign key
-	User       User   `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Foreign key relationship to User
-	Age        int    `json:"age"`                                             // User's age
-	Height     int    `json:"height"`                                          // User's height
-	Weight     int    `json:"weight"`                                          // User's weight
-	HeightUnit string `json:"height_unit"`                                     // Height unit (e.g., cm, inches)
-	WeightUnit string `json:"weight_unit"`                                     // Weight unit (e.g., kg, lbs)
-	Gender     string `json:"gender"`                                          // Gender of the user
-	Lifestyle  string `json:"lifestyle"`                                       // Lifestyle (e.g., sedentary, active)
+	UserID     string          `json:"user_id"`                                         // Foreign key
+	User       User            `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Foreign key relationship to User
+	Age        int             `json:"age"`                                             // User's age
+	Height     int             `json:"height"`                                          // User's height
+	Weight     int             `json:"weight"`                                          // User's weight
+	HeightUnit enum.HeightUnit `json:"height_unit"`                                     // Height unit (enum)
+	WeightUnit enum.WeightUnit `json:"weight_unit"`                                     // Weight unit (enum)
+	Gender     enum.Gender     `json:"gender"`                                          // Gender of the user
+	Lifestyle  enum.Lifestyle  `json:"lifestyle"`                                       // Lifestyle (enum)
 }
 
 // 5. Diet
 // Diet model linked to User
 type Diet struct {
 	gorm.Model
-	UserID          string    `json:"user_id"`                                         // Foreign key
-	User            User      `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Foreign key relationship to User
-	PreferredUnits  FoodUnits `json:"preferred_units" gorm:"type:jsonb"`               // Preferred units stored as JSONB
-	FavoriteRecipes []string  `json:"favorite_recipes" gorm:"type:jsonb"`              // Favorite recipes stored as JSONB
-	DislikedRecipes []string  `json:"disliked_recipes" gorm:"type:jsonb"`              // Disliked recipes stored as JSONB
-	FavoriteItems   []string  `json:"favorite_items" gorm:"type:jsonb"`                // Favorite food items stored as JSONB
-	DislikedItems   []string  `json:"disliked_items" gorm:"type:jsonb"`                // Disliked food items stored as JSONB
-	FavoriteCuisine []string  `json:"favorite_cuisine" gorm:"type:jsonb"`              // Favorite cuisines stored as JSONB
-	DislikedCuisine []string  `json:"disliked_cuisine" gorm:"type:jsonb"`              // Disliked cuisines stored as JSONB
+	UserID          string          `json:"user_id"`                                         // Foreign key
+	User            User            `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Foreign key relationship to User
+	PreferredUnits  FoodUnits       `json:"preferred_units" gorm:"type:jsonb"`               // Preferred units stored as JSONB
+	FavoriteRecipes []string        `json:"favorite_recipes" gorm:"type:jsonb"`              // Favorite recipes stored as JSONB
+	DislikedRecipes []string        `json:"disliked_recipes" gorm:"type:jsonb"`              // Disliked recipes stored as JSONB
+	FavoriteItems   []string        `json:"favorite_items" gorm:"type:jsonb"`                // Favorite food items stored as JSONB
+	DislikedItems   []string        `json:"disliked_items" gorm:"type:jsonb"`                // Disliked food items stored as JSONB
+	FavoriteCuisine []enum.Cuisines `json:"favorite_cuisine" gorm:"type:jsonb"`              // Favorite cuisines stored as JSONB
+	DislikedCuisine []enum.Cuisines `json:"disliked_cuisine" gorm:"type:jsonb"`              // Disliked cuisines stored as JSONB
 }
 
 // 6. Inventory
@@ -106,37 +106,28 @@ type Inventory struct {
 // Shopping model linked to User
 type Shopping struct {
 	gorm.Model
-	UserID                string `json:"user_id"`                                         // Foreign key
-	User                  User   `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Foreign key relationship to User
-	WillingnessToShop     bool   `json:"willingness_to_shop"`                             // Boolean for willingness to shop
-	BudgetCurrency        string `json:"budget_currency"`                                 // Currency (USD, EUR, etc.)
-	Budget                string `json:"budget"`                                          // Budget for shopping
-	EaseOfAvailability    string `json:"ease_of_availability"`                            // Ease of availability (hard, medium, easy)
-	ShoppingTimeAvailable string `json:"shopping_time"`                                   // Time available for shopping
+	UserID             string          `json:"user_id"`                                         // Foreign key
+	User               User            `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Foreign key relationship to User
+	ShoppingMode       enum.ShopMode   `json:"shopping_mode"`                                   // Boolean for willingness to shop
+	BudgetCurrency     enum.Currencies `json:"budget_currency"`                                 // Currency (USD, EUR, etc.)
+	Budget             int             `json:"budget"`                                          // Budget for shopping
+	EaseOfAvailability enum.Difficulty `json:"ease_of_availability"`                            // Ease of availability (hard, medium, easy)
+	ShoppingList       []FoodItem      `json:"shopping_list" gorm:"type:jsonb"`                 // Shopping list stored as JSONB
 }
 
 // 8. Goal
 // Goal model linked to User
 type Goal struct {
 	gorm.Model
-	UserID     string `json:"user_id"`                                         // Foreign key
-	User       User   `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Foreign key relationship to User
-	Goal       string `json:"goal"`                                            // User's goal (e.g., weight loss)
-	Target     string `json:"target"`                                          // Target (e.g., 10kg)
-	Deadline   string `json:"deadline"`                                        // Deadline for the goal
-	Preference string `json:"preference"`                                      // Preference (e.g., low carb, high protein)
-	Plan       string `json:"plan"`                                            // Plan to achieve the goal (e.g., diet, exercise)
-	Notes      string `json:"notes"`                                           // Notes related to the goal
-}
-
-// 9. Script
-// Script model linked to User
-type Script struct {
-	gorm.Model
-	UserID string `json:"user_id"`                                         // Foreign key
-	User   User   `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Foreign key relationship to User
-	Script string `json:"script"`                                          // Script text for LLM
-	Tokens int    `json:"tokens"`                                          // Number of tokens in the script
+	GoalID     string         `json:"goal_id" gorm:"primaryKey"`                       // Goal ID
+	UserID     string         `json:"user_id"`                                         // Foreign key
+	User       User           `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Foreign key relationship to User
+	Goal       enum.GoalTypes `json:"goal"`                                            // User's goal (e.g., weight loss)
+	Target     string         `json:"target"`                                          // Target (e.g., 10kg)
+	Deadline   string         `json:"deadline"`                                        // Deadline (datetime) for the goal
+	Preference string         `json:"preference"`                                      // Preference (e.g., low carb, high protein)
+	Plan       []string       `json:"plan" gorm:"type:jsonb"`                          // Plan to achieve the goal (step by step)
+	Notes      string         `json:"notes"`                                           // Notes related to the goal
 }
 
 // 10. Authentication
@@ -147,31 +138,20 @@ type Authentication struct {
 	LastUpdated string `json:"last_updated"`              // Last updated timestamp
 }
 
-// 11. LLM
-// LLM model linked to User
-//type LLM struct {
-//	gorm.Model
-//	UserID     string `json:"user_id"`                                         // Foreign key
-//	User       User   `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Foreign key relationship to User
-//	LLMChoice  string `json:"llm_choice"`                                      // LLM choice (e.g., GPT-3)
-//	LLMVersion string `json:"llm_version"`                                     // Version of the LLM used (e.g., GPT-3.5)
-//	LLMToken   string `json:"llm_token"`                                       // API token for LLM usage
-//}
-
 // 12. Recipe
 // Recipe model optionally linked to User
 type Recipe struct {
 	gorm.Model
 	RecipeID        string               `json:"recipe_id"`                                       // Recipe ID (can be a UUID or string)
 	Name            string               `json:"name"`                                            // Recipe name
-	Category        string               `json:"category"`                                        // Recipe category (e.g., appetizer, dessert)
-	Cuisine         string               `json:"cuisine"`                                         // Cuisine type (e.g., Italian, Mexican)
+	Tags            []enum.RecipeTags    `json:"tags" gorm:"type:jsonb"`                          // Recipe tags (e.g., appetizer, dessert)
+	Cuisine         enum.Cuisines        `json:"cuisine"`                                         // Cuisine type (e.g., Italian, Mexican)
 	Ingredients     []FoodItem           `json:"ingredients" gorm:"type:jsonb"`                   // List of ingredients (stored as JSONB)
 	Instructions    []RecipeInstructions `json:"instructions" gorm:"type:jsonb"`                  // Step-by-step instructions (stored as JSONB)
-	NutritionalInfo string               `json:"nutritional_info"`                                // Nutritional information
+	NutritionalInfo []FoodItem           `json:"nutritional_info" gorm:"type:jsonb"`              // Nutritional information
 	UserID          *string              `json:"user_id"`                                         // Optional Foreign key to the user who created the recipe (nullable)
 	User            *User                `json:"user" gorm:"foreignKey:UserID;references:UserID"` // Optional relationship to User
-	RecipeHTML      string               `json:"recipe_html"`                                     // HTML content of the recipe (for rendering purposes)
+	URL             string               `json:"url"`                                             // HTML content of the recipe (for rendering purposes)
 	Time            RecipeTime           `json:"recipe_time"`                                     // Time required for the recipe (prep time, cooking time)
 }
 
@@ -187,5 +167,5 @@ type MealChoice struct {
 	Innovative    bool   `json:"innovative"`                                      // Whether the user wants to try innovative recipes
 	Nutritional   string `json:"nutritional"`                                     // Nutritional preferences (e.g., high protein, low carb)
 	MealType      string `json:"meal_type"`                                       // Type of meal (e.g., breakfast, lunch, dinner)
-	FineTuned     bool   `json:"fine_tuned"`                                      // Whether fine-tuned recipes are required
+	FineTuned     bool   `json:"fine_tuned" default:"false"`                      // Whether fine-tuned recipes are required
 }
---
+++ backend/pkg/models/enum/README.md
@@ -0,0 +1,37 @@
+# How to define enums
+
+This package contains enums used throughout the ButlerAI backend. Enumerations are a set of named values that represent a specific type of data (think a distinct set of elements), making the code more readable and maintainable.
+
+
+Define each enum as follows:
+
+#### `enum/{{functional_area}}.go`
+```go
+package enum
+
+type Example int
+
+const (
+    option1 Example = iota
+    option2
+    option3
+)
+```
+
+#### `enum/{{functional_area}}_helper.go`
+```go
+package enum
+
+
+func (e enum.Example) String() string {
+    if e < enum.option1 || e > enum.option3 {
+        return "Unknown"
+    }
+    return [...]string{"Option 1", "Option 2", "Option 3"}[e]
+}
+```
+
+This way, the `Example` type now behaves as an enum, can be called in the codebase using the identifier `enum.Example`
+
+**When defining/editing enums ensure function is mapping the enums to the right values**
+
